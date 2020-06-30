db91517d4e0a5297bef334bd4c2adb0d6688ef72
aguaholic <joyterra@gmail.com>
2020-06-30 15:34:39 +0200

    Standardize buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,6 @@ import styled from 'styled-components'
 
 export const Button = styled.button`
     display: block;
-    color: ${props => props.theme.colors.mainYellow};
     padding: 0.5rem 1rem;
     text-transform: uppercase;
     font-size: 1.5rem;
@@ -12,8 +11,9 @@ export const Button = styled.button`
     margin: 2rem auto;
     transition: ${props => props.theme.transDefault};
     background: transparent;
+
     &:hover {
-        background: ${props => props.theme.mainWhite};
+        background-color: ${props => props.theme.colors.mainWhite};
         color: ${props => props.theme.colors.mainBlack};
         cursor: pointer;
     }
@@ -21,7 +21,7 @@ export const Button = styled.button`
 export const SectionButton = styled(Button)`
     font-size: 1.2rem;
     color: ${props => props.theme.colors.mainBlack};
-    border: ${props => props.theme.colors.mainBlack};
+    border: ${props => `0.15rem solid ${props.theme.colors.mainBlack}`};
 
     &:hover {
         background: ${props => props.theme.colors.mainBlack};
