7a8848adb86f79b5f3e1388b5d4d4c6a644b6264
aguaholic <joyterra@gmail.com>
2020-07-09 13:29:30 +0200

    Adjust Section component

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,9 +1,16 @@
 import styled from 'styled-components'
 
 const Section = styled.section`
-    padding: 2rem;
-    width: 90vw;
     margin: 0 auto;
+    padding: 0 15px;
+
+    @media (min-width: 576px) {
+        width: 95vw;
+    }
+
+    @media (min-width: 768px) {
+        width: 90vw;
+    }
 `
 
 export default Section
