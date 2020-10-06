ec6c75093231eb6ae833c8ffcbc4e9ac389ec594
aguaholic <joyterra@gmail.com>
2020-10-06 14:36:12 +0200

    Add correct url to icons

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -5,12 +5,12 @@ export default [
     {
         id: 0,
         icon: <FaFacebook className="icon facebook-icon" />,
-        url: 'https://facebook.com',
+        url: 'https://www.facebook.com/trajanopastelaria1',
     },
     {
         id: 1,
         icon: <FaInstagram className="icon instagram-icon" />,
-        url: 'https://instagram.com',
+        url: 'https://www.instagram.com/trajanopastelaria/',
     },
     {
         id: 2,
